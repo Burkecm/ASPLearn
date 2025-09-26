80f3d9627cdb9a05638d805d390f40b5428370d7
Burke <burkecm77@gmail.com>
2025-09-26 15:44:35 -0400

    Initial Commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new TestViewModel() { Name = "Chad", DOB = new DateOnly(1991, 05, 20)});
+        }
+    }
+}
