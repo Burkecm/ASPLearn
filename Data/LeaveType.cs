566fd41f6829cbd8400e306273cc66bf6e4414c9
Chad Burke <burkec@corning.com>
2025-10-14 11:06:53 -0400

    Added scaffolder CRUD ops for leave types, styled pages

---
+++ Data/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public int DaysAllocated { get; set; }
+    }
+}
