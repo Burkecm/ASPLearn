3b25dace1f802d740e2c9a9ba72af74c15c45399
Chad Burke <burkec@corning.com>
2025-10-29 13:01:53 -0400

    Refactored with VMs

---
+++ Models/LeaveTypes/BaseLeaveTypeVM.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models.LeaveTypes
+{
+    public abstract class BaseLeaveTypeVM
+    {
+        public Guid ID { get; set; }
+
+    }
+}
---
+++ Models/LeaveTypes/CreateVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Models.LeaveTypes
+{
+    public class CreateVM
+    {
+        [Required]
+        [Length(4, 150,  ErrorMessage = "Name length does not meet Requirements")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 90)]
+        [Display(Name="Number of Days")]
+        public int DaysAllocated { get; set; }
+    }
+}
